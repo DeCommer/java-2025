f10f63a279a1e05bca80feab86222c7e37e2de01
Joseph DeCommer <jdecommer@gmail.com>
2025-03-29 13:00:30 -0400

    java practice

--- src/N2025.java
+++ src/N2025.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class N2025 {
     
     public static void main(String[] args) {
         
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = in.nextLine();
+        System.out.println("Hello, " + name);
+
+        in.close();
     }
 } 
